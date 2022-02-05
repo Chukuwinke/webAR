a98dd16a40f10aa65a440e5c27430600d0bb6b96
Chukuwinke <rittnerodigwe@gmail.com>
2022-02-05 15:49:02 +0200

    wip

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# webAR
+# webXR Meta-Fasion web app
+## website uses WebXR Api to render 3d models on a website and in the real world
+### AR functionality currently only works for android device
--- js/script.js
+++ js/script.js
@@ -1,11 +1,48 @@
-$(document).ready(function() {
-    $('#autoWidth').lightSlider({
-        autoWidth:true,
-        loop:true,
-        pauseOnHover: true,
-        enableDrag: false,
-        onSliderLoad: function() {
-            $('#autoWidth').removeClass('cS-hidden');
-        } 
-    });  
-  });
+// $(document).ready(function() {
+//     $('#autoWidth').lightSlider({
+//         autoWidth:true,
+//         loop:true,
+//         pauseOnHover: true,
+//         enableDrag: false,
+//         onSliderLoad: function() {
+//             $('#autoWidth').removeClass('cS-hidden');
+//         } 
+//     });  
+//   });
+
+  const cardContainer = document.querySelector('.card-container')
+
+  const modelSrc = {
+      purpleGown: '3dmodel/dress4.glb',
+      jacket: '3dmodel/shearling_jacket/scene.gltf',
+      shoe: '3dmodel/free_air_jordan/scene.gltf',
+      addidas: '3dmodel/adidas_jacket/scene.gltf'
+  }
+  for(product in modelSrc ){
+      console.log(modelSrc[product])
+      const card = document.createElement("li")
+      card.classList.add("item-a", "col", "px-4" )
+      card.innerHTML += `
+      <!--BOX-SLIDER-------------------->
+      <div class="box ">
+        <a class="more-link"> go to description</a>
+        <!--IMAGE-ON-THE-BOX-->
+        <div class="slide-img">
+          <model-viewer class="w-100 model-window"
+            src="${modelSrc[product]}"
+            ar
+            ar-modes="webxr scene-viewer quick-look"
+            ar-scale="auto"
+            alt="A 3D model of the product"
+            auto-rotate
+            camera-controls
+          ></model-viewer>
+        </div>
+        <div class="info-container">
+          <span class="product-name">product</span>
+          <span class="price">$23</span>
+        </div>
+      </div>
+      `;
+      cardContainer.appendChild(card)
+  }
